30452df1f9d34567cf448f28cdabe7f1d93f0fa0
ACHMADA FIQRI A. RASYAD <38800157+fiqriachmada@users.noreply.github.com>
2019-10-11 09:18:18 +0700

    Update NPM07066Modul2_2Array.java

--- NPM07066Modul2_2Array/NPM07066Modul2_2Array.java
+++ NPM07066Modul2_2Array/NPM07066Modul2_2Array.java
@@ -1,32 +1,36 @@
-package NPM07066Modul2_2Array;
-
-import java.sql.SQLOutput;
-
-static class node{
-    int nip;
-    String nama;
-    String jabatan;
-    public node(int a,String b, String c){
-        this.nip=a;
-        this.nama=b;
-        this.jabatan=c;
-    }
-}
-static class node1{
-    node[]nod=new node[100];
-    int indeks=0;
-    void tambah(int npm, String nama, String jabatan){
-        nod[indeks]=new node(npm, nama, jabatan);
-        indeks=indeks+1;
-    }
-    void delok(){
-        System.out.println("= = = DATA PEGAWAI = = =");
-        for(int=0; i<indeks,i++){
-            System.out.println(nod[i].nip+".  "+nod[i].nama+"->"+nod[i].jabatan);
-        }
-    }    System.out.println("===================");
-}
-
 public class NPM07066Modul2_2Array {
+    static class node{
+        int nip;
+        String nama ,jabatan;
+        // String jabatan;
 
+        public node(int a,String b,String c){
+            this.nip=a;
+            this.nama=b;
+            this.jabatan=c;
+        }
+    }
+    static class node1{
+        node [] nod=new node[1000];
+        int indeks=0;
+        void tambah(int npm,String nama,String jabatan){
+            nod[indeks]= new node(npm,nama,jabatan);
+            indeks=indeks+1;
+        }
+        void view(){
+            System.out.println("===DATA PEGAWAI===");
+            for(int i=0;i<indeks;i++){
+                System.out.println(nod[i].nip+". "+nod[i].nama+"->"+nod[i].jabatan);
+            }
+            System.out.println("=============");
+        }
+    }
+    public static void main(String[] args) {
+        node1 nodd=new node1();
+        nodd.tambah(1, "andi", "programer");
+        nodd.tambah(2, "budi", "deisgner");
+        nodd.tambah(3, "cici", "PM");
+        nodd.tambah(4, "didi", "programer");
+        nodd.view();
+    }
 }
